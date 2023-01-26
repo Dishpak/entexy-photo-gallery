3db07f0be4e47b474010bd270e4cd7616e1d6604
Dishpak <djdishpak@gmail.com>
2023-01-26 14:29:32 +0300

    change routes & redirects to gh-pages

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -16,7 +16,7 @@ const AppRouter = () => {
       <Routes>
         {loggedIn && <Route path={'/gallery'} element={<Gallery/>}></Route>}
         {loggedIn && <Route path={baseUrl} element={<Navigate to={'/gallery'} />}></Route>}
-        {!loggedIn && <Route path={baseUrl} element={<Authorization/>}></Route>}
+        {!loggedIn && <Route path={`${baseUrl}/login`} element={<Authorization/>}></Route>}
         <Route path={'/login'} element={<Authorization />}></Route>
         {loggedIn && <Route path={'gallery/:id'} element={<ImageCard/>}></Route>}
       </Routes>
