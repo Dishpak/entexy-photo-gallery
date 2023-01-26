f1965b92438c822509df41d6f18a4796c55a808a
Dishpak <djdishpak@gmail.com>
2023-01-26 13:59:04 +0300

    change banner img path

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ToPictures from "./ToPictures";
+import bannerImg from '../images/banner-image.png'
 
 const Banner = () => {
   return (
     <div className={'banner'}>
-      <img src="/src/images/banner-image.png" alt=""/>
+      <img src={bannerImg} alt=""/>
       <div className={'bannerText'}>
         <h1>Lorem ipsum dolor sit amet</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
